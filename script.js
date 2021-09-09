3f84120d34ed69260f9cc3c46074ce2a3791c5cb
SheikhRashed <devrashed06@gmail.com>
2021-09-09 18:15:35 +0600

    password match function added

--- script.js
+++ script.js
@@ -54,16 +54,22 @@ function checkLength(input, min, max) {
 }
 
 // Check email is valid
-
 function checkEmail(input) {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  // return re.test(String(email).toLowerCase());
   if (re.test(input.value.trim())) {
     showSuccess(input);
   } else {
     showError(input, 'Email is not valid');
   }
+}
+
+// compare passwords is match or not
+
+function passwordMatch(input1, input2) {
+  input1.value !== input2.value
+    ? showError(input2, 'Passwords do not match')
+    : showSuccess(input2);
 }
 
 form.addEventListener('submit', (e) => {
@@ -74,4 +80,5 @@ form.addEventListener('submit', (e) => {
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
   checkEmail(email);
+  passwordMatch(password, cpassword);
 });
