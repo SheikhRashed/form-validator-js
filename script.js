1f69eb18e26b1da418afce8560f39fc874fad189
SheikhRashed <devrashed06@gmail.com>
2021-09-09 18:01:01 +0600

    refactored code

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 'use strict';
+
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const cpassword = document.querySelector('#confirmPassword');
+
+// Show Error
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show Success
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+// Get Field name
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Check Required Inputs
+function checkRequired(inputArr) {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Check Required & Validation
+  checkRequired([username, email, password, cpassword]);
+});
